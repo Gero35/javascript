0e3de3c03bb962d3c73255127527c3697136a697
Gero35 <geyurt@hotmail.com>
2024-10-16 17:00:08 -0500

    tesetr

---
+++ app.js
@@ -0,0 +1,25 @@
+let mensaje= "hola mundo"
+console.log(mensaje)
+mensaje ="chao mundo"
+console.log(mensaje)
+const PI = 3.1415
+console.log(PI)
+//PI = 1.2341
+//console.log(PI)
+let cantidad = 89
+let texto = "texto de prueba "
+let gameOver = false 
+let variableindefinida
+console.log(cantidad)
+//console.log(variableindefinida + gameOver)
+let lista = [3,6,8,9,3,7]
+console.log(lista[3])
+lista [6]= 18
+console.log(lista[6])
+let persona = {
+                nombre:"rosana",
+                apellidos:"aguliar",
+                edad:"17",
+                celular:"3127654622",
+                ciudad:"bucaramanga"};
+console.log(persona)
